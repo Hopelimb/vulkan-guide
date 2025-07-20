262020dbf9a1b27979a670c0f3c467e77549b5c2
Hopelimb <hopelimb@gmail.com>
2025-07-20 11:49:15 +0900

    Reapply "check point"
    This reverts commit 04226ef59ded8c143d3223fa378389b51bb2f684.

--- src/vk_descriptors.cpp
+++ src/vk_descriptors.cpp
@@ -1,1 +1,89 @@
 ﻿#include <vk_descriptors.h>
+
+void DsecriptorLayoutBuilder::add_binding(uint32_t binding, VkDescriptorType type)
+{
+	VkDescriptorSetLayoutBinding newbind
+	{
+		.binding = binding,
+		.descriptorType = type,
+		.descriptorCount = 1,
+	};
+	bindings.push_back(newbind);
+}
+
+void DsecriptorLayoutBuilder::clear()
+{
+	bindings.clear();
+}
+
+VkDescriptorSetLayout DsecriptorLayoutBuilder::build(VkDevice device, VkShaderStageFlags shaderStages, void* pNext, VkDescriptorSetLayoutCreateFlags flags)
+{
+	for (auto& binding : bindings)
+	{
+		binding.stageFlags |= shaderStages;
+	}
+	VkDescriptorSetLayoutCreateInfo info 
+	{
+		.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO,
+		.pNext = pNext,
+		.flags = flags,
+		.bindingCount = static_cast<uint32_t>(bindings.size()),
+		.pBindings = bindings.data(),
+	};
+
+	VkDescriptorSetLayout setlayout{};
+
+	VK_CHECK(vkCreateDescriptorSetLayout(device, &info, nullptr, &setlayout));
+
+	return setlayout;
+}
+
+void DescriptorAllocator::init_pool(VkDevice device, uint32_t mexSets, std::span<PoolSizeRatio> poolRatios)
+{
+	std::vector<VkDescriptorPoolSize> poolSizes;
+	for (const auto& ratio : poolRatios) 
+	{
+		poolSizes.push_back(
+			VkDescriptorPoolSize{
+			.type = ratio.type,
+			.descriptorCount = static_cast<uint32_t>(mexSets * ratio.ratio),
+		});
+	}
+
+	VkDescriptorPoolCreateInfo poolInfo 
+	{
+		.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO,
+		.maxSets = mexSets,
+		.poolSizeCount = static_cast<uint32_t>(poolSizes.size()),
+		.pPoolSizes = poolSizes.data(),
+	};
+
+	vkCreateDescriptorPool(device, &poolInfo, nullptr, &pool);
+}
+
+void DescriptorAllocator::clear_descriptors(VkDevice device)
+{
+	vkResetDescriptorPool(device, pool, 0);
+}
+
+void DescriptorAllocator::destroy_pool(VkDevice device)
+{
+	vkDestroyDescriptorPool(device, pool, nullptr);
+}
+
+VkDescriptorSet DescriptorAllocator::allocate(VkDevice device, VkDescriptorSetLayout layout)
+{
+
+	VkDescriptorSetAllocateInfo allocInfo
+	{
+		.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_ALLOCATE_INFO,
+		.pNext = nullptr,
+		.descriptorPool = pool,
+		.descriptorSetCount = 1,
+		.pSetLayouts = &layout,
+	};
+
+	VkDescriptorSet ds;
+	VK_CHECK(vkAllocateDescriptorSets(device, &allocInfo, &ds));
+	return ds;
+}
