c18c994a4dfb000a2483fad22dfc03af8c156ab2
Hopelimb <hopelimb@gmail.com>
2025-07-20 11:49:10 +0900

    Reapply "Check point"
    This reverts commit 84c92a7b374e26d3398d21d816f76561d04249e6.

--- src/vk_engine.h
+++ src/vk_engine.h
@@ -5,8 +5,39 @@
 
 #include <vk_types.h>
 
+struct DeletionQueue
+{
+	std::deque<std::function<void()>> _deletors;
+
+	void push_function(std::function<void()> && function) {
+		_deletors.push_back(std::move(function));
+	}
+
+	void flush()
+	{
+		for (auto it = _deletors.rbegin(); it != _deletors.rend(); it++)
+		{
+			(*it)();
+		}
+
+		_deletors.clear();
+	}
+};
+
+struct FrameData {
+	VkCommandPool _commandPool{ VK_NULL_HANDLE };
+	VkCommandBuffer _mainCommandBuffer{ VK_NULL_HANDLE };
+	VkSemaphore _swapchainSemaphore{ VK_NULL_HANDLE }, _renderSemaphore{ VK_NULL_HANDLE };
+	VkFence _renderFence{ VK_NULL_HANDLE };
+
+	DeletionQueue _deletionQueue;
+};
+
+constexpr unsigned int FRAME_OVERLAP = 2; // number of frames in flight
+
 class VulkanEngine {
 public:
+	const uint64_t TIMEOUT = 1000000000; // 1 second in nanoseconds
 
 	bool bUseValidationLayers{ true };
 	bool _isInitialized{ false };
@@ -26,9 +57,21 @@ public:
 	VkSwapchainKHR _swapchain{ VK_NULL_HANDLE };
 	VkFormat _swapchainImageFormat{ VK_FORMAT_UNDEFINED };
 
-	std::vector<VkImage> _swapchainImages;
-	std::vector<VkImageView> _swapchainImageViews;
+	std::vector<VkImage> _swapchainImages{};
+	std::vector<VkImageView> _swapchainImageViews{};
 	VkExtent2D _swapchainExtent{ 0, 0 };
+
+	FrameData _frames[FRAME_OVERLAP];
+	FrameData& get_current_frame() { return _frames[_frameNumber % FRAME_OVERLAP]; };
+
+
+	VkQueue _graphicsQueue{ VK_NULL_HANDLE };
+	uint32_t _graphicsQueueFamily{ 0 };
+
+	DeletionQueue _mainDeletionQueue{};
+	VmaAllocator _allocator{ VK_NULL_HANDLE };
+	AllocatedImage _drawImage{};
+	VkExtent2D _drawExtent{};
 
 	//initializes everything in the engine
 	void init();
@@ -38,6 +81,7 @@ public:
 
 	//draw loop
 	void draw();
+	void draw_background(VkCommandBuffer cmd);
 
 	//run main loop
 	void run();
